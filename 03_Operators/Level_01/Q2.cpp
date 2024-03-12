bd518c35c2fc79d3f779ec49c9ec76f57ace631d
aftab1038 <aftabk.1038@outlook.com>
2024-03-12 19:51:28 +0500

    Operators

---
+++ 03_Operators/Level_01/Q2.cpp
@@ -0,0 +1,30 @@
+// Code by AFTAB NAFEES
+// GitHub : aftab1038
+
+/*  Q2. Write a program to input the value of the radius of a circle from keyboard and then calculate its perimeter and area. */
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{ 
+    // variable declaration
+    const float pi = 3.14;
+    float radius, area, perimeter;
+
+    // taking input from user (radius)
+    cout<<"Enter the radius of Circle : ";
+    cin>>radius;
+
+    // calculating area and perimeter
+    area = pi*pow(radius, 2);
+    perimeter = 2*pi*radius;
+
+    // Printing out area and perimeter
+    cout<<"Area of Circle : "<<area<<endl;
+    cout<<"Circumference(perimeter) of Circle : "<<perimeter<<endl;
+
+
+}
