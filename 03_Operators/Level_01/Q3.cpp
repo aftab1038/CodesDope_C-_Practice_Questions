a9bce3e2ff823829a01db5711d7d52ff86bab665
aftab1038 <aftabk.1038@outlook.com>
2024-03-13 18:29:25 +0500

    Operators

---
+++ 03_Operators/Level_01/Q3.cpp
@@ -0,0 +1,14 @@
+// Code by AFTAB NAFEES
+// GitHub : aftab1038
+
+/*  Q3.  Write a program to enter a 4 digit number from the keyboard. Add 8 to the number and then divide it by 3. Now, the modulus of that number is taken with 5 and then multiply the resultant value by 5. Display the final result. */
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{ 
+
+}
--- 03_Operators/Level_02/Q2.cpp
+++ 03_Operators/Level_02/Q2.cpp
@@ -9,21 +9,36 @@ using namespace std;
 
 int main()
 {
+    // Getting 4 digit number from user
     int number;
     cout<<"Enter 4 digit number : ";
     cin>>number;
     
-    int num1 = (number / 1000)+2;
+    // Breaking into each digit
+    int num1 = (number / 1000);
     number = number % 1000;
-    int num2 = (number / 100)+2;
+    int num2 = (number / 100);
     number = number % 100;
     int num3 = number / 10;
     int num4 = number % 10;
 
+    // Adding 2 to each digit
+    num1 += 2;
+    num2 += 2;
+    num3 += 2;
+    num4 += 2;
+    
+    // If any digit increase by 10 then do consider unit digit
+    if (num1 >= 10 && num2 >= 10 && num3 >= 10 && num4 >= 10)
+    {
+        num1 = num1 % 10;
+        num2 = num2 % 10;
+        num3 = num3 % 10;
+        num4 = num4 % 10;
+    }
 
-cout<<"Num 1 : "<<num1<<endl;
-cout<<"Num 2 : "<<num2<<endl;
-cout<<"Num 3 : "<<num3<<endl;
-cout<<"Num 4 : "<<num4<<endl;
+    // Printing the number after adding 2 to each digit
+    cout<<"New number : "<<num1<<num2<<num3<<num4<<endl;
+    
     return 0;
 }
