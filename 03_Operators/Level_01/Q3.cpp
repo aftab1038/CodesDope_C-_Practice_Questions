640c85d08506ea883e243c6f2cf08d0c0633fb33
aftab1038 <aftabk.1038@outlook.com>
2024-03-13 18:50:40 +0500

    Operators

--- 03_Operators/Level_01/Q3.cpp
+++ 03_Operators/Level_01/Q3.cpp
@@ -10,5 +10,19 @@ using namespace std;
 
 int main()
 { 
+    // taking input from user
+    int number;
+    cout<<"Enter a 4-digit Number : ";
+    cin>>number;
+    
+    // Operation on inputed number
+    number += 8;    // Adding 8
+    number /= 3;    // Dividing it by 3
+    number %= 5;    // Modulus of it with 5
+    number *= 5;    // Multiply resultant by 5
 
+    // Display the result
+    cout<<"Result : "<<number<<endl;
+
+    return 0;
 }
